e018bce7e64cbd5dd5f7e81765958db1c059f93f
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-18 22:34:39 +0530

    install react,  react-dom and add browserslist in package json

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+    React.createElement("h1", {}, "I am rr tag"),
+    React.createElement("h2", {}, "I am hero tag"),
   ]),
   //another children
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+    React.createElement("h2", {}, "I am hero tag"),
   ]),
 ]);
 
