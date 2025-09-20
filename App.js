9c9a8fa4a24756d833e301a997b3a1d0e1cfc476
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-20 21:07:35 +0530

    update parcel-cache and dist to delete old code

--- App.js
+++ App.js
@@ -1,26 +1,2 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am rr tag"),
-    React.createElement("h2", {}, "I am hero tag"),
-  ]),
-  //another children
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am hero tag"),
-  ]),
-]);
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from React!"
-);
-
-// console.log(heading); //return js object
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
