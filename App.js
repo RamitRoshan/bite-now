1ebec7220376e0abc29918f7ca5d0adba69dac7c
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-21 13:14:47 +0530

    implement component composition and implement jsx inside fn component

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+ 
+
+const elem = <span>React Element </span>
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    {elem}
+    React Js using - JSX🚀
+  </h1>
+);
+
+ 
+
+//React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+      {Title()}
+    <h1 className="heading">React Functional Component</h1>
+  </div>
+  
+);
+
+const Header = () => <h1>Bangalore</h1>;
+const Content =() => <p>tech city bengaluru</p>
+const Page = () => {
+  return(
+    <div>
+      <Header/>
+      <Content/>
+    </div>
+  )
+}
 
 
-//JSX - Html or XML like
-const jsxHeading = <h1 id="heading">React Js using JSX</h1>
 
-console.log(jsxHeading);
+// console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent/>);
+ 
+
+
+ 
