16cac96542e06a232cd69af2efa315e8652db4b9
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-23 09:20:16 +0530

    implement functional component

--- App.js
+++ App.js
@@ -2,38 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
  
 
-const elem = <span>React Element </span>
 
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    {elem}
-    React Js using - JSX🚀
-  </h1>
-);
-
- 
-
-//React Functional Component
-const HeadingComponent = () => (
-  <div id="container">
-      {Title()}
-    <h1 className="heading">React Functional Component</h1>
-  </div>
-  
-);
-
-const Header = () => <h1>Bangalore</h1>;
-const Content =() => <p>tech city bengaluru</p>
-const Page = () => {
-  return(
-    <div>
-      <Header/>
-      <Content/>
+const Header = () => {
+  return (
+    <div className="header">
+      <div></div>
     </div>
   )
 }
-
-
+ 
+//App components
+const AppLayout = () => {
+  return (
+    <div className="app"></div>
+  )
+}
+ 
 
 // console.log(jsxHeading);
 
