9d68d425ce6f49d56d3a81a90846fc652c5ff6ac
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-23 16:11:09 +0530

    adds nav items and logo in this web app

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
- 
 
 
 const Header = () => {
   return (
     <div className="header">
-      <div></div>
+      <div className="logo-container">
+        <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   )
 }
@@ -23,7 +32,7 @@ const AppLayout = () => {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<Header/>);
  
 
 
