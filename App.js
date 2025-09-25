c89845526e1a69e9aa8754ec0b724ceeaa68e7c4
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-26 02:14:44 +0530

    Implements maps and key to show restaurant card

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ const RestaurantCard = (props) => {
   );
 };
 
-
+//resList is an array
 const resList = [
   {
     "info": {
@@ -1329,23 +1329,12 @@ const Body = () => {
   return (
     <div className="body">
       <div className="search">Search</div>
-      <div className="res-container">
-        <RestaurantCard resData = {resList[0]}/>
-        <RestaurantCard resData = {resList[1]}/>
-        <RestaurantCard resData = {resList[2]}/>
-        <RestaurantCard resData = {resList[3]}/>
-        <RestaurantCard resData = {resList[4]}/>
-        <RestaurantCard resData = {resList[5]}/>
-        <RestaurantCard resData = {resList[6]}/>
-        <RestaurantCard resData = {resList[7]}/>
-        <RestaurantCard resData = {resList[8]}/>
-        <RestaurantCard resData = {resList[9]}/>
-        <RestaurantCard resData = {resList[10]}/>
-        <RestaurantCard resData = {resList[11]}/>
-        <RestaurantCard resData = {resList[12]}/>
-        <RestaurantCard resData = {resList[13]}/>
+      <div className="res-container">     
         
-        
+          {/* //using map to show the restaurant(can use for loop also), instead of writing individual code */}
+          {resList.map((restaurant) => (
+            <RestaurantCard key={restaurant.info.id} resData = {restaurant}/>
+          ))};
       </div>
     </div>
   );
