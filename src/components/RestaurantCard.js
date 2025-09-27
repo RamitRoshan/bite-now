14bfefadc055ce9e51cb0bde329f40dfdb90974c
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-28 02:08:49 +0530

    shift all diff codes in diff file of components

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,19 @@
+const RestaurantCard = (props) => {
+  // destructuring data
+  const {resData} = props;
+  const {cloudinaryImageId, name, avgRating, cuisines, costForTwo, sla: {slaString}} = resData?.info; 
+  return (
+    <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
+      <img className="res-logo"
+      alt="res-logo" 
+      src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" + cloudinaryImageId}/>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>Rating: {avgRating}⭐</h4>
+      <h4>Cost: {costForTwo}</h4>
+      <h4>Delivery: {slaString}</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
