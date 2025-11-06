2e3d693d3698fc9d62ba629c3de0a1be916bb0c1
Ramit Roshann <ramitroshan777777@gmail.com>
2025-11-06 21:52:59 +0530

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,8 @@ const Contact = () => {
     return (
         <div>
             <h1>Contact Us Page</h1>
+            <h1>Contact Us Page</h1>
+            <h1>Contact Us Page</h1>
             
         </div>
     );
