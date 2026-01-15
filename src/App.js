8479d3019573ffa451688215cae29ecdac1ad43c
Ramit Roshann <ramitroshan777777@gmail.com>
2026-01-15 13:47:46 +0530

    implement lazy loading to optimize our app and made a multiple bundle for main code, for About sections and for Grocery also, here along with lazy we use suspense and fallback as a placeholder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import "../index.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import Grocery from "./components/Grocery";
 
+//lazy loading of Grocery
+const Grocery = lazy(() => import("./components/Grocery")); 
+
+//lazy loading of About us
+const About = lazy(() => import("./components/About"));
 
 //App components
 const AppLayout = () => {
@@ -33,12 +39,25 @@ const appRouter = createBrowserRouter([
       {
         //if my path is '/about', load the about page 
         path: "/about",
-        element: <About/>,
+        // element: <About/>,
+        element: (
+        <Suspense fallback ={<h1>Loading....</h1>}>
+          <About/>
+        </Suspense>
+        ),
       },
       {
         //if my path is '/contact', load the contact page 
         path: "/contact",
         element: <Contact/>,
+      },
+      { 
+        path: "/grocery",
+        element: (
+        <Suspense fallback ={<h1>Loading....</h1>}>
+          <Grocery/>
+        </Suspense>
+        ),
       },
       {
         //this part of url has a Dynamics path, as every restaurant has different data
