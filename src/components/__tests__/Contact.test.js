e12b492db3e12bc0ca2c626c2ddbb4b0f853511e
Ramit Roshann <ramitroshan777777@gmail.com>
2026-02-05 20:43:36 +0530

    create a jest config file and now run the test case for contact component to check heading is loading or not

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,13 +2,19 @@
 
 import { render, screen } from "@testing-library/react"
 import Contact from "../Contact"
+import "@testing-library/jest-dom";
 
 test("Should load contact us component", () => {
     //render this contact on JS-DOM
     render(<Contact/>);
 
-    //it will render heading here
-    const heading = screen.getByRole("heading");
+    //it will render heading here, we have multiple 
+    // heading, so to test it in contact we will 
+    // choose one heading 
+    const heading = screen.getByRole("heading", {
+        level: 1,
+        name: /get in touch/i,
+    });
 
     //Assertion
     expect(heading).toBeInTheDocument();
